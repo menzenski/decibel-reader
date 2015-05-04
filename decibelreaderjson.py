1dee8f8f47dfaaf2f76259074340237abaebf957
Matt Menzenski <matt.menzenski@gmail.com>
2015-05-04 17:31:50 -0500

    json files are now updated properly

--- decibelreaderjson.py
+++ decibelreaderjson.py
@@ -4,6 +4,7 @@
 #import usb.core
 import time
 import json
+import os.path
 
 ## initialize
 #dev = usb.core.find(idVendor=0x16c0, idProduct=0x5dc)
@@ -22,6 +23,20 @@ def read_decibels():
             print str(lastdb)
             return lastdb
 
+def create_new_json(title):
+    """Create a new json file."""
+    empty_json = "[]"
+    json_filename = "{}.json".format(title)
+    with open(json_filename, "w") as stream:
+        stream.write(empty_json)
+
+def check_json(title):
+    """Create a json file if one doesn't already exist."""
+    if not os.path.isfile("{}.json".format(title)):
+        create_new_json(title)
+    else:
+        pass
+
 def new_entry():
     entry_date = time.strftime("%Y/%m/%d")
     entry_time = time.strftime("%H:%M:%S")
@@ -29,13 +44,19 @@ def new_entry():
     # entry_decibels = ## read something
     return {"date": entry_date, "time": entry_time, "db": entry_decibels}
 
+def update_json(json_file):
+    
+    with open(json_file, "r") as j:
+        data = json.load(j)
+
+    data.append(new_entry())
+
+    with open(json_file, "w") as j:
+        j.write(json.dumps(data, indent=4))
+
             
 if __name__ == "__main__":
-    #get_date_and_time()
-    #read_decibels()
-    #create_new_xml_file("testxml")
-    mydata = []
-    entry = new_entry()
-    mydata.append(entry)
-    with open('mydata.json', 'w') as f:
-        f.write(json.dumps(mydata))
+    my_title = "fancyjson"
+    check_json(my_title)
+    update_json("{}.json".format(my_title))
+    
